5f2e33333becebe3be419b71f19cd1895ce21cd5
Nikolai Shvechkov <vivalastelmah@gmail.com>
2025-05-02 12:54:13 +0300

    feat: parse cli cmd func

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
 import { styleText } from "node:util";
-import { capitalize } from "./utils/index.js";
+import { capitalize, showCurrentDir, parseCliCmd } from "./utils/index.js";
 
-function main() {
+(async function main() {
   process.username = capitalize(process.env.npm_config_username);
 
   if (!process.username) {
     console.warn(
       `Specify user name in args to start using FileManager! ${styleText(
         "grey",
-        "e.g. npm run start -- --username=goa"
+        "(e.g. npm run start -- --username=goa)"
       )}`
     );
 
     return;
   }
 
-  console.log(`Welcome to the File Manager, ${process.username}!`);
+  console.log(`Welcome to the File Manager, ${process.username}!\n`);
+  const userNoticeMsg =
+    "To proceed enter command in cli and wait for execution results.\n";
 
-}
+  console.log(userNoticeMsg);
+  console.log(showCurrentDir());
 
-main();
+  process.stdin.on("data", (data) => {
+    console.log(parseCliCmd(data.toString()));
+
+    // on end
+    process.stdout.write(`${userNoticeMsg}${showCurrentDir()}\n`);
+  });
+
+  // process.stdin.pipe(process.stdout);
+  // await pipeline(process.stdin, process.stdout);
+})();
---
+++ src/utils/parseCliCmd.js
@@ -0,0 +1,15 @@
+export const parseCliCmd = (data) => {
+  const [cmd, ...rest] = data.trim().split(" ");
+
+  const args = rest.reduce(
+    (acc, item) => {
+      if (item.startsWith("--")) acc.flags.push(item);
+      else acc.args.push(item);
+
+      return acc;
+    },
+    { args: [], flags: [] }
+  );
+
+  return { cmd, ...args };
+};
