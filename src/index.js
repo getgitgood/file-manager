bfdeef5f55fd5367c86cc353c6bfd66262f8b61e
Nikolai Shvechkov <vivalastelmah@gmail.com>
2025-05-02 11:25:55 +0300

    feat: parse username and display greeting msg

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
-(function main() {
-  const username = process.argv.slice(2);
-  console.log(username);
-})();
+import { styleText } from "node:util";
+import { capitalize } from "./utils/index.js";
+
+function main() {
+  process.username = capitalize(process.env.npm_config_username);
+
+  if (!process.username) {
+    console.warn(
+      `Specify user name in args to start using FileManager! ${styleText(
+        "grey",
+        "e.g. npm run start -- --username=goa"
+      )}`
+    );
+
+    return;
+  }
+
+  console.log(`Welcome to the File Manager, ${process.username}!`);
+
+}
+
+main();
