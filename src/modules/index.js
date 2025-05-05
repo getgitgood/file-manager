7c886294ae7684202cd0cbda0149da92b3ce9261
Nikolai Shvechkov <vivalastelmah@gmail.com>
2025-05-05 12:16:26 +0300

    feat: hash is done

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,3 +2,4 @@ export { navigate } from "./navigate.js";
 export { dispatch } from "./dispatch.js";
 export { files } from "./files.js";
 export { system } from "./system.js";
+export { hash } from "./hash.js";
